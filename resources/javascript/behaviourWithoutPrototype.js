30317bc8933d01364c0ea07f6b9e46b9e0dbb9f2
Justinas Rakita <justinas@idega.com>
2007-05-03 18:49:57 +0000

    behaviour based on mootools

---
+++ resources/javascript/behaviourWithoutPrototype.js
@@ -0,0 +1,29 @@
+var BehaviourBaseClass = new Class({
+    initialize: function(){
+        this.behaviours = [];
+        var bhvr = this;
+        window.addEvent('domready', function(){bhvr.apply()});
+    },
+    register: function(actions){
+        if(! this.behaviours.test(actions))
+            this.behaviours.push(actions);
+    },
+    apply: function(actions) {
+        if ($type(actions)!='array') {
+            actions = this.behaviours;
+        }
+        actions.each(function(bhvrs){
+            for (bhvr in bhvrs){
+                try {
+                    if($type(bhvrs[bhvr])=='function') {
+                        $S(bhvr).each(function(el){
+                            bhvrs[bhvr](el);
+                        });
+                    }
+                } catch(e){}
+            }
+        });
+    }
+});
+var Behaviour = new BehaviourBaseClass();
+Behaviour.initialize();
