e5ce180b3999449823f38ba9aa221a98ea009a6a
Valdas Zemaitis <valdas@idega.com>
2008-05-22 08:08:59 +0000

    *** empty log message ***

--- src/java/com/idega/block/web2/business/JQueryUIType.java
+++ src/java/com/idega/block/web2/business/JQueryUIType.java
@@ -2,15 +2,16 @@ package com.idega.block.web2.business;
 
 /**
  * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  *
- * Last modified: $Date: 2008/02/14 15:48:25 $ by $Author: civilis $
+ * Last modified: $Date: 2008/05/22 08:08:59 $ by $Author: valdas $
  */
 public enum JQueryUIType  {
 	
 	UI_TABS {public String getFileName() { return "ui.tabs.js"; }},
 	UI_TABS_CSS {public String getFileName() { return "themes/flora/flora.tabs.css"; }},
-	UI_TABS_EXT {public String getFileName() { return "ui.tabs.ext.js"; }};
+	UI_TABS_EXT {public String getFileName() { return "ui.tabs.ext.js"; }},
+	UI_ACCORDION {public String getFileName() { return "ui.accordion.js"; }};
 	
 	public abstract String getFileName();
 }
