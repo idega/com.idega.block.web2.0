73ed1644070ef37d50018866fb6ecb1051829f90
Valdas Žemaitis <valdas@idega.com>
2009-09-17 18:24:21 +0200

    Added UUID for the class

--- src/java/com/idega/block/web2/servlet/ImageCaptchaServlet.java
+++ src/java/com/idega/block/web2/servlet/ImageCaptchaServlet.java
@@ -3,7 +3,6 @@ package com.idega.block.web2.servlet;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
@@ -16,13 +15,16 @@ import com.octo.captcha.service.CaptchaServiceException;
 import com.sun.image.codec.jpeg.JPEGCodec;
 import com.sun.image.codec.jpeg.JPEGImageEncoder;
 
-@SuppressWarnings("restriction")
 public class ImageCaptchaServlet extends HttpServlet {
 
+	private static final long serialVersionUID = -8970660545568735821L;
+
+	@Override
 	public void init(ServletConfig servletConfig) throws ServletException {
 		super.init(servletConfig);
 	}
 
+	@Override
 	protected void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
 		byte[] captchaChallengeAsJpeg = null;
 		// the output stream to render the captcha image as jpeg into
