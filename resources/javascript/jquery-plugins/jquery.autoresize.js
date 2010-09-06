95517edf45af0c8ae8a15c4c435c453083ecd143
Valdas Žemaitis <valdas@idega.com>
2010-09-06 19:45:37 +0200

    Auto initializing non-empty textareas with the text

--- resources/javascript/jquery-plugins/jquery.autoresize.js
+++ resources/javascript/jquery-plugins/jquery.autoresize.js
@@ -5,9 +5,7 @@
  */
 
 ;(function($){
-    
     $.fn.autoResize = function(options) {
-        
         // Just some abstracted details,
         // to make plugin users happy:
         var settings = $.extend({
@@ -21,8 +19,7 @@
         
         // Only textarea's auto-resize:
         this.filter('textarea').each(function(){
-            
-                // Get rid of scrollbars and disable WebKit resizing:
+            // Get rid of scrollbars and disable WebKit resizing:
             var textarea = $(this).css({resize:'none','overflow-y':'hidden'}),
             
             	line_height	= 16 || parseInt(textarea.css('line-height'));
@@ -70,37 +67,39 @@
                     if (clone.html() != html) {
 						clone.html(html);
 					
-                    // Find the height of text:
-                   	cloneHeight = clone.scrollTop();
-                   	if (cloneHeight == 0) {
-                   		cloneHeight = clone.height();
-                   	}
-                    var scrollTop = Math.max(cloneHeight, origHeight) + settings.extraSpace;
-					var toChange = $(this);
+	                    // Find the height of text:
+	                   	cloneHeight = clone.scrollTop();
+	                   	if (cloneHeight == 0) {
+	                   		cloneHeight = clone.height();
+	                   	}
+	                    var scrollTop = Math.max(cloneHeight, origHeight) + settings.extraSpace;
+						var toChange = $(this);
+							
+	                    // Don't do anything if scrollTip hasen't changed:
+	                    if (lastScrollTop === scrollTop) {
+	                    	AutoResize.executeCustomCallback(event);
+	                    	return;
+	                    }
+	                    lastScrollTop = scrollTop;
 						
-                    // Don't do anything if scrollTip hasen't changed:
-                    if (lastScrollTop === scrollTop) {
+	                    // Check for limit:
+	                    if (scrollTop >= settings.limit) {
+	                        $(this).css('overflow-y','');
+	                        AutoResize.executeCustomCallback(event);
+	                        return;
+	                    }
+	                    // Fire off callback:
+	                    settings.onResize.call(this);
+						
+	                    // Either animate or directly apply height:
+	                    settings.animate && textarea.css('display') === 'block' ?
+	                        toChange.stop().animate({height:scrollTop}, settings.animateDuration, function() {
+	                        	settings.animateCallback();
+	                        	AutoResize.executeCustomCallback(event);
+	                        })
+	                        : toChange.height(scrollTop);
+                    } else {
                     	AutoResize.executeCustomCallback(event);
-                    	return;
-                    }
-                    lastScrollTop = scrollTop;
-					
-                    // Check for limit:
-                    if ( scrollTop >= settings.limit ) {
-                        $(this).css('overflow-y','');
-                        AutoResize.executeCustomCallback(event);
-                        return;
-                    }
-                    // Fire off callback:
-                    settings.onResize.call(this);
-					
-                    // Either animate or directly apply height:
-                    settings.animate && textarea.css('display') === 'block' ?
-                        toChange.stop().animate({height:scrollTop}, settings.animateDuration, function() {
-                        	settings.animateCallback();
-                        	AutoResize.executeCustomCallback(event);
-                        })
-                        : toChange.height(scrollTop);
                     }
                 };
             
@@ -110,16 +109,11 @@
                 .bind('keyup.dynSiz', updateSize)
                 .bind('keydown.dynSiz', updateSize)
                 .bind('change.dynSiz', updateSize);
-            
         });
         
         // Chain:
         return this;
-        
     };
-    
-    
-    
 })(jQuery);
 
 AutoResize = {};
@@ -128,3 +122,26 @@ AutoResize.executeCustomCallback = function(event) {
 		event.callback();
 	}
 }
+
+jQuery(window).load(function() {
+	jQuery('textarea').each(function() {
+		var textArea = jQuery(this);
+		
+		if (FluxInterfaceHelper && !FluxInterfaceHelper.isTextAreaHtmlEditor(textArea)) {
+			var value = textArea.attr('value');
+			if (value != null && value != '') {
+				textArea.attr('value', value + '$$initializer$$');	//	Setting dummy text
+				
+				var keyDownEvent = jQuery.Event('keydown');
+		    	keyDownEvent.callback = function() {
+			    	var customTextIndex = value.length - 1;
+			   		if (customTextIndex > 0) {
+			   			IWCORE.setCaretToPos(textArea[0], customTextIndex);
+			   		}
+			   		textArea.attr('value', value);	//	Restoring original text
+			   	}
+		    	jQuery(textArea).trigger(keyDownEvent);
+			}
+		}
+	});
+});
