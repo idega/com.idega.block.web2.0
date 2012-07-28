274752072941de6a3eec48eb67704024d119dd41
Thorhallur Helgason <laddih@gmail.com>
2012-07-28 23:10:28 +0000

    Removing unnecessary @Override annotations

--- src/java/com/idega/block/web2/business/WebResourcePluginsResolver.java
+++ src/java/com/idega/block/web2/business/WebResourcePluginsResolver.java
@@ -16,27 +16,22 @@ public class WebResourcePluginsResolver implements Map<String, String> {
 	@Autowired
 	private JQuery jQuery;
 
-	@Override
 	public void clear() {
 		throw new UnsupportedOperationException(this.getClass().getName() + ": UnsupportedOperationException");
 	}
 
-	@Override
 	public boolean containsKey(Object key) {
 		throw new UnsupportedOperationException(this.getClass().getName() + ": UnsupportedOperationException");
 	}
 
-	@Override
 	public boolean containsValue(Object value) {
 		throw new UnsupportedOperationException(this.getClass().getName() + ": UnsupportedOperationException");
 	}
 
-	@Override
 	public Set<java.util.Map.Entry<String, String>> entrySet() {
 		throw new UnsupportedOperationException(this.getClass().getName() + ": UnsupportedOperationException");
 	}
 
-	@Override
 	public String get(Object key) {
 		if (Web2BusinessBean.JQUERY_FOLDER_NAME_PREFIX.equals(key))
 			return jQuery.getBundleURIToJQueryLib();
@@ -48,39 +43,31 @@ public class WebResourcePluginsResolver implements Map<String, String> {
 		return key.toString();
 	}
 
-	@Override
 	public boolean isEmpty() {
 		throw new UnsupportedOperationException(this.getClass().getName() + ": UnsupportedOperationException");
 	}
 
-	@Override
 	public Set<String> keySet() {
 		throw new UnsupportedOperationException(this.getClass().getName() + ": UnsupportedOperationException");
 	}
 
-	@Override
 	public String put(String key, String value) {
 		throw new UnsupportedOperationException(this.getClass().getName() + ": UnsupportedOperationException");
 	}
 
-	@Override
 	public void putAll(Map<? extends String, ? extends String> t) {
 		throw new UnsupportedOperationException(this.getClass().getName() + ": UnsupportedOperationException");
 	}
 
-	@Override
 	public String remove(Object key) {
 		throw new UnsupportedOperationException(this.getClass().getName() + ": UnsupportedOperationException");
 	}
 
-	@Override
 	public int size() {
 		throw new UnsupportedOperationException(this.getClass().getName() + ": UnsupportedOperationException");
 	}
 
-	@Override
 	public Collection<String> values() {
 		throw new UnsupportedOperationException(this.getClass().getName() + ": UnsupportedOperationException");
 	}
-
 }
