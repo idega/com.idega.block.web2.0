db8fe8e2a21c4e65ff618a3b0591378f5b425404
amzinas <zygimantas@idega.com>
2015-03-12 15:27:25 +0200

    Localizations for TinyMCE.

---
+++ resources/javascript/tinymce/3.5.8/langs/sv_OLD.js
@@ -0,0 +1,154 @@
+tinyMCE.addI18n({en:{
+common:{
+edit_confirm:"Vill du använda WYSIWYG-läget för denna textarea?",
+apply:"Verkställ",
+insert:"Infoga",
+update:"Uppdatera",
+cancel:"Avboka",
+close:"Stäng",
+browse:"Bläddra",
+class_name:"Klass",
+not_set:"-- Inte satt --",
+clipboard_msg:"Kopiera/Klipp ut/Klistra är inte tillgänglig i Mozilla och Firefox.\nVill du vill ha mer information om det här problemet?",
+clipboard_no_support:"För närvarande inte stöds av din webbläsare, använda kortkommandon i stället.",
+popup_blocked:"Tyvärr, men vi har märkt att din popup-blockerare har inaktiv ett fönster som ger ansökan funktionalitet. Du måste inaktivera popup blockering på denna webbplats för att till fullo utnyttja detta verktyg.",
+invalid_data:"Fel: Ogiltiga värden in, dessa är markerade i rött.",
+more_colors:"Fler färger"
+},
+contextmenu:{
+align:"Anpassning",
+left:"Vänster",
+center:"Centrum",
+right:"Höger",
+full:"Full"
+},
+insertdatetime:{
+date_fmt:"%Y-%m-%d",
+time_fmt:"%H:%M:%S",
+insertdate_desc:"Infoga datum",
+inserttime_desc:"Infoga tid",
+months_long:"Januari,Februari,Mars,April,Maj,Juni,Juli,Augusti,September,Oktober,November,December",
+months_short:"Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Okt,Nov,Dec",
+day_long:"Söndag,Måndag,Tisdag,Onsdag,Torsdag,Fredag,Lördag,Söndag",
+day_short:"Sön,Mån,Tis,Ons,Tor,Fre,Lör,Sön"
+},
+print:{
+print_desc:"Skriv ut"
+},
+preview:{
+preview_desc:"Förhandsgranska"
+},
+directionality:{
+ltr_desc:"Riktning vänster till höger",
+rtl_desc:"Riktning höger till vänster"
+},
+layer:{
+insertlayer_desc:"Infoga nytt lager",
+forward_desc:"Flytta framåt",
+backward_desc:"Flytta bakåt",
+absolute_desc:"Växla absolut positionering",
+content:"Nytt lager..."
+},
+save:{
+save_desc:"Spara",
+cancel_desc:"Avbryt alla ändringar"
+},
+nonbreaking:{
+nonbreaking_desc:"Sätt fast mellanslag tecken"
+},
+iespell:{
+iespell_desc:"Kör stavningskontroll",
+download:"ieSpell inte upptäcks. Vill du installera det nu?"
+},
+advhr:{
+advhr_desc:"Horisontell regel"
+},
+emotions:{
+emotions_desc:"Känslor"
+},
+searchreplace:{
+search_desc:"Hitta",
+replace_desc:"Hitta/Ersätt"
+},
+advimage:{
+image_desc:"Infoga/redigera bild"
+},
+advlink:{
+link_desc:"Infoga/redigera länk"
+},
+xhtmlxtras:{
+cite_desc:"Citat",
+abbr_desc:"Förkortning",
+acronym_desc:"Akronym",
+del_desc:"Radering",
+ins_desc:"Införande",
+attribs_desc:"Infoga/redigera attribut"
+},
+style:{
+desc:"Redigera CSS Stil"
+},
+paste:{
+paste_text_desc:"Klistra in som oformaterad text",
+paste_word_desc:"Klistra in från Word",
+selectall_desc:"Markera allt"
+},
+paste_dlg:{
+text_title:"Användning CTRL+V på tangentbordet för att klistra in texten i fönstret.",
+text_linebreaks:"Behåll radbrytningar",
+word_title:"Användning CTRL+V på tangentbordet för att klistra in texten i fönstret."
+},
+table:{
+desc:"Infogar en ny tabell",
+row_before_desc:"Infoga rad före",
+row_after_desc:"Infoga rad efter",
+delete_row_desc:"Ta bort rad",
+col_before_desc:"Infoga kolumn innan",
+col_after_desc:"Infoga kolumn efter",
+delete_col_desc:"Avlägsna kolonn",
+split_cells_desc:"Sträck fusione tabellceller",
+merge_cells_desc:"Slå ihop tabellceller",
+row_desc:"Tabellrad fastigheter",
+cell_desc:"Tabell cellegenskaper",
+props_desc:"Tabellegenskaper",
+paste_row_before_desc:"Klistra tabellrad före",
+paste_row_after_desc:"Klistra tabellrad efter",
+cut_row_desc:"Klipp tabellrad",
+copy_row_desc:"Kopiera tabellrad",
+del:"Bort tabell",
+row:"Rad",
+col:"Kolumn",
+cell:"Cellen"
+},
+autosave:{
+unload_msg:"De ändringar du har gjort kommer att förloras om du navigerar bort från denna sida."
+},
+fullscreen:{
+desc:"Växla helskärmsläge"
+},
+media:{
+desc:"Infoga / redigera inbäddad media",
+edit:"Redigera inbäddad media"
+},
+fullpage:{
+desc:"Dokumentegenskaper"
+},
+template:{
+desc:"Infoga fördefinierad mall innehåll"
+},
+visualchars:{
+desc:"Visuella styrtecken på / av."
+},
+spellchecker:{
+desc:"Växla stavningskontroll",
+menu:"Stavningskontroll-inställningar",
+ignore_word:"Ignorera ord",
+ignore_words:"Ignorera allt",
+langs:"Språk",
+wait:"Vänta...",
+sug:"Förslag",
+no_sug:"Inga förslag",
+no_mpell:"Inga stavfel hittades."
+},
+pagebreak:{
+desc:"Infoga sidbrytning."
+}}});
